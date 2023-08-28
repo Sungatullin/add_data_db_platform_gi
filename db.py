3d8d29dc9eb7e7f9eac66c8972e7f747708c4227
Sungatullin Bulat <sungatullinbulat05@gmail.com>
2023-08-28 17:52:48 +0300

    init

---
+++ db.py
@@ -0,0 +1,147 @@
+import psycopg2
+
+
+# транслитерация латиницы на кириллицу
+def transliteration(string_object: str):
+    # словарь замен
+    letters = {'a': 'а', 'b': 'в', 'c': 'с', 'd': 'д', 'e': 'е', 'f': 'ф', 'g': 'г', 'h': 'н', 'k': 'к', 'l': 'л',
+               'm': 'м', 'n': 'н', "o": "о", 'r': 'р', 'p': 'п', 's': 'с', 't': 'т', 'u': 'у', 'v': 'в', 'x': 'х',
+               'z': 'з', 'y': 'у', 'j': 'ю', 'iii': '3', 'ii': '2', 'i': '1', ".": "", " ": "", "-": "", '"': "",
+               "№": ""}
+
+    # применение словаря замен к названию
+    if type(string_object) is str:
+        string_object = string_object.lower()
+        for letter in letters.keys():
+            string_object = string_object.replace(letter, letters[letter])
+        return string_object
+    return string_object
+
+
+class DB:
+    def __init__(self):
+        self.conn = psycopg2.connect(dbname='geo_atlas_dp_bulat', user='postgres',
+                                     password='bulat2002', host='localhost')
+        self.cur = self.conn.cursor()
+
+    def close_con(self):
+        self.cur.close()
+        self.conn.close()
+
+    def add_company(self, company_name, other_names):
+        other_names = '{' + f"{','.join(other_names)}" + '}'
+        self.cur.execute(
+            '''INSERT INTO users_company (other_names, name) VALUES ({!r}, {!r}) 
+               on conflict do nothing'''.format(f'{other_names}', company_name)
+        )
+        self.conn.commit()
+
+    def link_user_with_company(self):
+        self.cur.execute(
+            f'''SELECT id FROM users_user''')
+        user_id = self.cur.fetchone()[0]
+        self.cur.execute(
+            f'''SELECT id FROM users_company''')
+        company_id = self.cur.fetchone()[0]
+        print(user_id, company_id)
+        self.cur.execute(
+            f'''UPDATE users_user SET company_id = {company_id} WHERE id = {user_id}'''
+        )
+        self.conn.commit()
+
+    def add_one_field(self, field_name, kod,  company_name, other_names):
+        self.cur.execute(
+            f'''SELECT id FROM users_company WHERE name = '{company_name}' ''')
+        company_id = self.cur.fetchone()[0]
+        self.cur.execute(
+            '''INSERT INTO rest_field (name, kod, company_id, other_names) VALUES ({!r}, {}, {}, {!r}) 
+               on conflict do nothing'''.format(field_name, kod, company_id, other_names)
+        )
+        self.conn.commit()
+
+    def add_one_horizon(self, full_name, other_names, company_name, kod):
+        self.cur.execute(
+            f'''SELECT id FROM users_company WHERE name = '{company_name}' ''')
+        company_id = self.cur.fetchone()[0]
+        self.cur.execute(
+            '''INSERT INTO rest_horizon (full_name, company_id, other_names, kod) 
+               VALUES ({!r}, {}, {!r}, {}) on conflict do nothing'''.format(
+               full_name, company_id, other_names, kod)
+        )
+        self.conn.commit()
+
+    def add_one_layer(self, full_name, other_names, company_name, kod):
+        self.cur.execute(
+            f'''SELECT id FROM users_company WHERE name = '{company_name}' ''')
+        company_id = self.cur.fetchone()[0]
+        try:
+            hor_kod = kod // 100
+            self.cur.execute(
+                f'''SELECT id FROM rest_horizon WHERE kod = {hor_kod}''')
+            horizon_id = self.cur.fetchone()[0]
+            xy = self.cur.execute(
+                '''INSERT INTO rest_layer (full_name, company_id, horizon_id, other_names, kod)
+                   VALUES ({!r}, {}, {}, {!r}, {}) on conflict do nothing'''.format(
+                   full_name, company_id, horizon_id, other_names, kod)
+            )
+            self.conn.commit()
+        except Exception as e:
+            pass
+
+    def add_one_area(self, field_name: str, area_name: str, company_name):
+        self.cur.execute(
+            f'''SELECT id FROM users_company WHERE name = '{company_name}' ''')
+        company_id = self.cur.fetchone()[0]
+        try:
+            field_name = transliteration(field_name)
+            self.cur.execute(
+                f'''SELECT id FROM rest_field WHERE '{field_name}' = Any(other_names)''')
+            field_id = self.cur.fetchone()[0]
+            other_names = "{" + transliteration(area_name) + "}"
+            xy = self.cur.execute(
+                '''INSERT INTO rest_area (name, company_id, field_id, other_names)
+                    VALUES ({!r}, {}, {}, {!r}) on conflict do nothing'''.format(
+                   area_name, company_id, field_id, other_names)
+            )
+            self.conn.commit()
+        except Exception as e:
+            pass
+
+    def add_ngdu_shop(self, field_name: str, kod, ngdu_name: str, company_name, shops: list):
+        self.cur.execute(
+            f'''SELECT id FROM users_company WHERE name = '{company_name}' ''')
+        company_id = self.cur.fetchone()[0]
+        try:
+            field_name = transliteration(field_name)
+            self.cur.execute(
+                f'''SELECT id FROM rest_field WHERE '{field_name}' = Any(other_names)''')
+            field = self.cur.fetchone()
+            self.cur.execute(
+                f'''SELECT id FROM rest_ngdu WHERE '{kod}' = kod''')
+            ngdu = self.cur.fetchone()
+            if ngdu is None:
+                other_names_ngdus = "{" + transliteration(ngdu_name) + "}"
+                self.cur.execute(
+                    '''INSERT INTO rest_ngdu (name, kod, company_id, other_names)
+                       VALUES ({!r}, {}, {}, {!r}) on conflict do nothing'''.format(
+                       ngdu_name, kod, company_id, other_names_ngdus)
+                )
+                self.cur.execute(
+                    f'''SELECT id FROM rest_ngdu WHERE '{kod}' = kod''')
+                ngdu = self.cur.fetchone()
+            self.cur.execute(
+                '''INSERT INTO rest_ngdu_fields (ngdu_id, field_id)
+                   VALUES ({}, {}) on conflict do nothing'''.format(
+                    ngdu[0], field[0])
+            )
+            for shop in shops:
+                other_names_shops = "{" + transliteration(shop) + "}"
+                self.cur.execute(
+                    '''INSERT INTO rest_workshop (name, company_id, ngdu_id, other_names)
+                       VALUES ({!r}, {}, {}, {!r}) on conflict do nothing'''.format(
+                        shop, company_id, ngdu[0], other_names_shops)
+                )
+            self.conn.commit()
+
+        except:
+            pass
---
+++ main.py
@@ -0,0 +1,60 @@
+import pandas as pd
+from db import DB, transliteration
+import json
+
+fields = "fields.json"
+areas = "areas.json"
+horizons = "horizons.json"
+layers = "layers.json"
+ngdus = "ngdus.json"
+company_name = "Татнефть"
+other_names_ = ['татнефть', 'tatneft']
+
+database = DB()
+
+
+def add_data():
+    # добавление месторождений
+    with open(fields, "r", encoding="utf8") as fields_:
+        elems: dict = json.load(fields_)
+    for row in elems:
+        other_names = "{" + f"{','.join(row['other_names'])}" + "}"
+        database.add_one_field(kod=row["kod"], field_name=row["field_name"],
+                               other_names=other_names, company_name=company_name)
+
+    # добавление площадей
+    with open(areas, "r", encoding="utf8") as areas_:
+        elems: dict = json.load(areas_)
+    for key, values in elems.items():
+        for area in values:
+            database.add_one_area(field_name=key, area_name=area, company_name=company_name)
+
+    # добавление нгду и цехов
+    with open(ngdus, "r", encoding="utf8") as ngdus_:
+        elems: dict = json.load(ngdus_)
+    for row in elems:
+        for field in row["fields"]:
+            database.add_ngdu_shop(field_name=field, kod=row["kod"], ngdu_name=row["name"],
+                                   company_name=company_name, shops=row["shop"])
+
+    # добавление горизонтов
+    with open(horizons, "r", encoding="utf8") as horizons_:
+        elems: dict = json.load(horizons_)
+    for row in elems:
+        other_names = "{" + f"{','.join(row['other_names'])}" + "}"
+        database.add_one_horizon(full_name=row["horizon_name"], other_names=other_names,
+                                 company_name=company_name, kod=row["kod"])
+
+    # добавление пластов
+    with open(layers, "r", encoding="utf8") as layers_:
+        elems: dict = json.load(layers_)
+    for row in elems:
+        other_names = "{" + f"{','.join(row['other_names'])}" + "}"
+        database.add_one_layer(full_name=row["layer_name"], other_names=other_names,
+                               company_name=company_name, kod=row["kod"])
+
+
+if __name__ == '__main__':
+    # add_data()
+    database.add_company(company_name=company_name, other_names=other_names_)
+    database.link_user_with_company()
